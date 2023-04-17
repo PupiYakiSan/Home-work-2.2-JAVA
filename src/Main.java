317df35c8ad259989d2a6f3492d629103b9b7db0
Olga <o.n.p@mail.ru>
2023-04-17 16:13:40 +0300

    Change class Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int account = 100;
+        int refill = 1100;
+        int bonus = 100;
+        int score;
+
+        if (refill > 1000) {
+            score = refill / bonus;
+            refill = refill + score;
+        } else {
+            score = 0;
+        }
+
+        account = account + refill;
+
+        System.out.println("Итоговый счет = " + account + " руб.");
+        System.out.println("Бонусные рубли = " + score+ " руб.");
+
     }
 }
